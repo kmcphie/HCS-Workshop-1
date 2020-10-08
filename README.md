8050dfe9f6d517b54715d03ed36e3b18fc88bc6e
Katherine McPhie <katherinemcphie@college.harvard.edu>
2020-10-07 21:48:35 -0400

    Updated Experience tab

--- README.md
+++ README.md
@@ -4,12 +4,12 @@ Welcome to my website!
 
 ### Added Education Tab
 
-I added an additional education tab to the website.
+I created an Education tab and listed my education there the way it would be listed on a resume.
 
 Screenshots
 
-### Added Feature #2
+### Added Descriptions to Experience Tab
 
-Description
+I added a descriptions to each of the things in the Experience tab so that when you click on one, not only does it show up bigger on the highlighted portion, but more information about the experience also shows up below that.
 
-Screenshots
+![Image of Experience Tab]("/Desktop/Coding/HCS_Comp/HCS-Workshop-1/src/assets/experience_tab_ss.png")
--- src/views/Experience/Experience.jsx
+++ src/views/Experience/Experience.jsx
@@ -4,7 +4,8 @@ import "./Experience.css";
 function TimeLineItem(props) {
   return (
     <div onClick = {() => {
-      props.setActiveTitle(props.title);}}
+      props.setActiveTitle(props.title);
+      props.setActiveDescription(props.description);}}
       className = "timeline-item">
       <h4>{props.title}</h4>
     </div>
@@ -12,35 +13,51 @@ function TimeLineItem(props) {
 }
 
 export default function ExperienceScreen() {
-  const [activeTitle, setActiveTitle] = React.useState("");
+  const [activeTitle, setActiveTitle] = React.useState("Click on an experience to see more details!");
+  const [activeDescription, setActiveDescription] = React.useState("");
 
   return (
     <div className="screen-background">
       <div className="experience-background">
         <div className="timeline-background">
           <TimeLineItem 
-            title = "Google Computer Science Summer Institute: Online" 
-            setActiveTitle = {setActiveTitle}>
+            title = "Google Computer Science Summer Institute: Online"
+            description = "Learned introductory project-based Javascript processing curriculum taught by Google engineers" 
+            setActiveTitle = {setActiveTitle}
+            setActiveDescription = {setActiveDescription}>
           </TimeLineItem>
           <TimeLineItem 
             title = "International Space Settlement Design Competition"
-            setActiveTitle = {setActiveTitle}>
+            description = "Won 2nd Place at Kennedy Space Center’s 4-day competition"
+            setActiveTitle = {setActiveTitle}
+            setActiveDescription = {setActiveDescription}>
           </TimeLineItem>
           <TimeLineItem 
             title = "Research Intern in Computational Mathematics at University of California, Irvine"
-            setActiveTitle = {setActiveTitle}>
+            description = "Modeled and analyzed behavior of unstructured proteins using MATLAB"
+            setActiveTitle = {setActiveTitle}
+            setActiveDescription = {setActiveDescription}>
           </TimeLineItem>
           <TimeLineItem 
-            title = "Mathematics Intensive Summer Session at California State University, Fullerton"
-            setActiveTitle = {setActiveTitle}>
+            title = "Mathematics Intensive Summer Session (MISS) - California State University, Fullerton"
+            description = "Earned top accolade of “MISS Mathematician” in free, intensive, competitive, 4-week full-day program"
+            setActiveTitle = {setActiveTitle}
+            setActiveDescription = {setActiveDescription}>
           </TimeLineItem>
           <TimeLineItem 
             title = "AppJam+ Regional Mobile App Development Competition"
-            setActiveTitle = {setActiveTitle}>
+            description = "Won 2nd place in Southern California with four teammates in semester-long creation of app"
+            setActiveTitle = {setActiveTitle}
+            setActiveDescription = {setActiveDescription}>
           </TimeLineItem>
         </div>
-        <div className="highlight-background">
-          <h1>{activeTitle}</h1>
+        <div className = "info-background">
+          <div className="highlight-background">
+            <h1>{activeTitle}</h1>
+          </div>
+          <div className="more-info">
+            <h3>{activeDescription}</h3>
+          </div>
         </div>
       </div>
     </div>
